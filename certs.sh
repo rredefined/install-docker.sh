a8e68e96688a50f407cb8c5d6a0ef3a49c751636
Redefined <freakgaming2009@gmail.com>
2025-09-13 18:56:11 +0530

    Update and rename install-docker.sh to certs.sh

---
+++ certs.sh
@@ -0,0 +1,3 @@
+mkdir -p /etc/certs
+cd /etc/certs
+openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=NA/ST=NA/L=NA/O=NA/CN=Generic SSL Certificate" -keyout privkey.pem -out fullchain.pem
--- install-docker.sh
+++
@@ -1,7 +0,0 @@
-apk update
-apk add docker
-rc-update add docker
-service docker start
-clear
-docker run --rm hello-world
-docker image rm hello-world:latest
